650e319feeac2e06eb23206978fe6f2d175ca315
Liz429 <144296593+Liz429@users.noreply.github.com>
2023-10-07 13:32:16 +0700

    Update Kepsek.php

--- Kepsek.php
+++ Kepsek.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html> 
+<html>
 <head>
-   <title>Kepsek</title>
+   <title>kepsek</title>
 </head>
 <body>
-   <?php
+   <?
    session_start();
 
-   // cek apakah yang mengakses halaman ini sudah login 
-if($_SESSION['level']==""){ 
-   header("location:index.php?pesan=gagal");
+   // cek apakah yang mengakses halaman ini sudah login
+if($_SESSION['level']==""){	
+   header("location:index.php? pesan=gagal");
 }
 
 ?>
-<h1>Halaman Kepala Sekolah</h1>
+<h1>kepsek</h1>
 
-<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b>
-<?php echo $_SESSION['level']; ?></b>.</p>
+<p>Halo <b><?php  echo $_SESSION['username']; ?><b> Anda telah login sebagai <b>
+<? echo $_SESSION['level']; ?></b>.</p>
 <a href="logout.php">LOGOUT</a>
 
 <br/>
