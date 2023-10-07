28beb8b7b573cc69722b4b6e892149bd8a0f7df0
Liz429 <144296593+Liz429@users.noreply.github.com>
2023-10-07 13:35:21 +0700

    Update Orang tua.php

--- Orang tua.php
+++ Orang tua.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html> 
+<html>
 <head>
    <title>orangtua</title>
 </head>
 <body>
-   <?php
+   <?
    session_start();
 
-   // cek apakah yang mengakses halaman ini sudah login 
-if($_SESSION['level']==""){ 
-   header("location:index.php?pesan=gagal");
+   // cek apakah yang mengakses halaman ini sudah login
+if($_SESSION['level']==""){
+   header("location:index.php? pesan=gagal");
 }
 
 ?>
-<h1>Halaman Orang tua</h1>
+<h1>orangtua</h1>
 
-<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b>
-<?php echo $_SESSION['level']; ?></b>.</p>
+<p>Halo <b><?php  echo $_SESSION['username']; ?><b> Anda telah login sebagai <b>
+<? echo $_SESSION['level']; ?></b>.</p>
 <a href="logout.php">LOGOUT</a>
 
 <br/>
