97c43fb8ae6db6a736fa6a044c092fc4264ccf99
Liz429 <144296593+Liz429@users.noreply.github.com>
2023-10-07 13:25:16 +0700

    Update Koneksi.php

--- Koneksi.php
+++ Koneksi.php
@@ -1,8 +1,9 @@
 <?php 
 $koneksi = mysqli_connect("localhost","root","","Db_website");
 
-// Check connection
+// Check connection 
 if (mysqli_connect_errno()){
-  echo "Koneksi database gagal : ". mysqli_connect_error();
+ echo "Koneksi database gagal : " . mysqli_connect_error(); 
 }
+
  ?>
