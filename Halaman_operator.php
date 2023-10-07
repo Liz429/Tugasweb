5024388b1e9b3f7a37afbae6d5c0f27bd4fcd764
Liz429 <144296593+Liz429@users.noreply.github.com>
2023-10-07 13:28:32 +0700

    Update Halaman_operator.php

--- Halaman_operator.php
+++ Halaman_operator.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
-<html> 
+<html>
 <head>
-   <title>Halaman Operator</title>
+   <title>operator</title>
 </head>
 <body>
-   <?php
+   <?
    session_start();
 
-   // cek apakah yang mengakses halaman ini sudah login 
-if($_SESSION['level']==""){ 
-   header("location:index.php?pesan=gagal");
+   // cek apakah yang mengakses halaman ini sudah login
+if($_SESSION['level']==""){
+   header("location:index.php? pesan=gagal");
 }
 
 ?>
-<h1>Halaman Operator</h1>
+<h1>operator</h1>
 
-<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b>
-<?php echo $_SESSION['level']; ?></b>.</p>
+<p>Halo <b><?php  echo $_SESSION['username']; ?><b> Anda telah login sebagai <b>
+<? echo $_SESSION['level']; ?></b>.</p>
 <a href="logout.php">LOGOUT</a>
 
 <br/>
